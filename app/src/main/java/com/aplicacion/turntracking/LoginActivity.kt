3ff8b326a03aad0f75dfa37dded4bb4e470f8b37
Alejandro Pagani <alejandro.pagani@rappi.com>
2020-05-22 18:04:57 -0300

    Feature | New activity login | Alejandro pagani

--- app/src/main/java/com/aplicacion/turntracking/LoginActivity.kt
+++ app/src/main/java/com/aplicacion/turntracking/LoginActivity.kt
@@ -2,17 +2,12 @@ package com.aplicacion.turntracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-       // Thread.sleep(1000)
-
         setTheme(R.style.AppTheme) //le seteo nuevamente el estilo de la aplicaciomn porqie al iniciar tiene el style del splash
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
